3e649b83b5afb509a6a03e46921ce9df16952c38
xXSamXx00 <samudichio@gmail.com>
2021-10-15 13:03:54 +0200

    Creata struttura prima parte

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+const items = ['img/01.jpg', 'img/02.jpg', 'img/03.jpg', 'img/04.jpg', 'img/05.jpg'];
+
+const titles = ['Svezia', 'Svizzera', 'Gran Bretagna', 'Germania', 'Paradise']
+
+const texts = ['Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.', 'Lorem ipsum', 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.', 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,', 'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam']
+
+const itemsTitlesTexts = [...items, ...titles, ...texts]
+
+for (let i = 0; i < itemsTitlesTexts.length; i++) {
+    const elements = itemsTitlesTexts[i];
+    
+}
