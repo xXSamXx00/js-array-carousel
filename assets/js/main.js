08642c5224b3f03d6cd71ec14cba86b9b97aea88
xXSamXx00 <samudichio@gmail.com>
2021-10-15 16:09:03 +0200

    Seconda parte js da fixare

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,19 +2,17 @@
 const items = ['img/01.jpg', 'img/02.jpg', 'img/03.jpg', 'img/04.jpg', 'img/05.jpg'];
 const titles = ['Svezia', 'Svizzera', 'Gran Bretagna', 'Germania', 'Paradise']
 const texts = ['Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.', 'Lorem ipsum', 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.', 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,', 'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam']
-// Gli unisco con spread
-const itemsTitlesTexts = [...items, ...titles, ...texts]
 // Prendo da Html gli elementi
-const heroImage = document.querySelector(".hero_image")
 const buttonSlideUp = document.getElementById("slide_up")
 const buttonSlideDown = document.getElementById("slide_down")
-//  
+// 
 buttonSlideUp.addEventListener("click", function() {
-    for (let i = 0; i < itemsTitlesTexts.length; i++) {
-        const elements = itemsTitlesTexts[i];
-        console.log(elements);
+    const heroImage = document.querySelector(".hero_image>img")
+    for (let i = 0; i < items.length; i++) {
+        const elements = items[i];
+        heroImage.setAttribute("src", `./${elements}`)
+        console.log(heroImage);
     }
-
 })
 
 buttonSlideDown.addEventListener("click", function() {
