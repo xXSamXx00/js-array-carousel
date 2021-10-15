2c4ff084cf3524a552356b98890f19305221de4d
xXSamXx00 <samudichio@gmail.com>
2021-10-15 15:11:38 +0200

    Prima parte js da fixare

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,22 @@
+// Inserisco tutti gli array
 const items = ['img/01.jpg', 'img/02.jpg', 'img/03.jpg', 'img/04.jpg', 'img/05.jpg'];
-
 const titles = ['Svezia', 'Svizzera', 'Gran Bretagna', 'Germania', 'Paradise']
-
 const texts = ['Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.', 'Lorem ipsum', 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.', 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,', 'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam']
-
+// Gli unisco con spread
 const itemsTitlesTexts = [...items, ...titles, ...texts]
+// Prendo da Html gli elementi
+const heroImage = document.querySelector(".hero_image")
+const buttonSlideUp = document.getElementById("slide_up")
+const buttonSlideDown = document.getElementById("slide_down")
+//  
+buttonSlideUp.addEventListener("click", function() {
+    for (let i = 0; i < itemsTitlesTexts.length; i++) {
+        const elements = itemsTitlesTexts[i];
+        console.log(elements);
+    }
 
-for (let i = 0; i < itemsTitlesTexts.length; i++) {
-    const elements = itemsTitlesTexts[i];
-    
-}
+})
+
+buttonSlideDown.addEventListener("click", function() {
+
+})
