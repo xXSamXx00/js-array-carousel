ef48b2adf33ddad8a5e4734157a2d97494789679
xXSamXx00 <samudichio@gmail.com>
2021-10-18 13:00:17 +0200

    Aggiunto loop infinito da fixare immagine active

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,20 +9,43 @@ const heroImage = document.querySelector(".hero_image>img")
 const sweden = document.getElementById("svezia")
 sweden
 const itemElement = document.querySelector(".items")
+const titleElement = document.getElementById("title")
+const textElement = document.getElementById("text")
 // 
 for (let i = 0; i < items.length; i++) {
     const item = `<img src="${items[i]}">`;
     itemElement.innerHTML += item
 }
 
+document.querySelectorAll(".items")[0].classList.add("select_image")
+document
+
 let plusOne = 0
 heroImage.setAttribute("src", `./${items[0]}`)
 
-buttonSlideUp.addEventListener("click", function() {
-    heroImage.setAttribute("src", `./${items[++plusOne]}`) 
 
+buttonSlideUp.addEventListener("click", function() {
+    if (plusOne == 0) {
+        plusOne = items.length - 1
+    } else {
+        plusOne = plusOne - 1
+    }
+
+    heroImage.setAttribute("src", `./${items[plusOne]}`)
+    
+    document.querySelector(".items.select_image").classList.remove("select_image")
+    document.querySelectorAll(".items")[plusOne].classList.add("select_image")
 })
 
 buttonSlideDown.addEventListener("click", function() {
-    heroImage.setAttribute("src", `./${items[--plusOne]}`)
+    if (plusOne == items.length - 1) {
+        plusOne = 0
+    } else {
+        plusOne = plusOne + 1
+    }
+    
+    heroImage.setAttribute("src", `./${items[plusOne]}`)
+
+
+
 })
